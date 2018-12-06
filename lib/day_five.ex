c21d9719edb8640585f32f03b68af96fdcf10165
Chris Keathley <c@keathley.io>
2018-12-05 22:23:43 -0500

    Day Five part two

--- lib/day_five.ex
+++ lib/day_five.ex
@@ -1,7 +1,28 @@
 defmodule Aoc.DayFive do
+  @letters ~w|a b c d e f g h i j k l m n o p q r s t u v w x y z|
+
   def part_one(input) do
     input
     |> String.graphemes
+    |> chain_react
+    |> Enum.count
+  end
+
+  def part_two(input) do
+    chars =
+      input
+      |> String.graphemes
+
+    @letters
+    |> Enum.map(fn c -> fn -> create_permutation(chars, c) end end)
+    |> Enum.map(&Task.async/1)
+    |> Enum.map(& Task.await(&1, 20_000))
+    |> Enum.min
+  end
+
+  def create_permutation(chars, char) do
+    chars
+    |> Enum.reject(& String.downcase(&1) == char)
     |> chain_react
     |> Enum.count
   end
@@ -16,6 +37,7 @@ defmodule Aoc.DayFive do
     end
   end
 
+  def react([], acc), do: Enum.reverse(acc)
   def react([a], acc), do: Enum.reverse([a | acc])
   def react([a, b | rest], acc) do
     if reactable?(a, b) do
